07c6447d6b0bbb05f516aa4227f754334f991055
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-07-23 20:07:23 +0100

    feat: update casks

--- scripts/install_brew_packages.sh
+++ scripts/install_brew_packages.sh
@@ -6,21 +6,18 @@ brew update
 brew install --cask 1password
 brew install --cask adobe-acrobat-reader
 brew install atuin
+brew install --cask brave-browser
+brew install --cask cursor
 brew install --cask docker
-brew install --cask dropbox
-brew install --cask google-chrome
 brew install htop
 brew install --cask notion
-brew install pre-commit
 brew install --cask qbittorrent
 brew install r
 brew install rustup-init
-brew install --cask slack
 brew install --cask spotify
 brew install starship
 brew install --cask sublime-text
 brew install --cask vlc
-brew install --cask visual-studio-code
 brew install --cask whatsapp
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
